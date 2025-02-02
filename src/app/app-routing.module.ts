7ab00d14a585faa1fc551b85bb65dfd6f46415a0
nikhilkumbhare <nikhilk190697@gmail.com>
2025-02-02 20:03:09 +0530

    added changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectCreateFormComponent } from './components/project-create-form/project-create-form.component';
+import { SvgImageGeneratorComponent } from './components/svg-image-generator/svg-image-generator.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'create-project', component: ProjectCreateFormComponent },
+  { path: 'help', component: SvgImageGeneratorComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MY_DATE_FORMATS } from './date-formats';
+import { DatePipe } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectCreateFormComponent } from './components/project-create-form/project-create-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectDetailsComponent } from './components/project-details/project-details.component';
+import { TankDetailsComponent } from './components/tank-details/tank-details.component';
+import { TopCoverDetailsComponent } from './components/top-cover-details/top-cover-details.component';
+import { SvgImageGeneratorComponent } from './components/svg-image-generator/svg-image-generator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectCreateFormComponent,
+    HeaderComponent,
+    ProjectDetailsComponent,
+    TankDetailsComponent,
+    TopCoverDetailsComponent,
+    SvgImageGeneratorComponent
   ],
   imports: [
     BrowserModule,
@@ -27,9 +45,17 @@ import { HomeComponent } from './components/home/home.component';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
